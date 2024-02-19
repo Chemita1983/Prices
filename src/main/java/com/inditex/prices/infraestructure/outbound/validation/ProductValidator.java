cdf9e0deb6dcaa311a5854c7d30d0e58179cb351
Chemita1983 <josemjimenezvega@gmail.com>
2024-02-19 16:10:37 +0100

    change error message

--- src/main/java/com/inditex/prices/infraestructure/outbound/validation/ProductValidator.java
+++ src/main/java/com/inditex/prices/infraestructure/outbound/validation/ProductValidator.java
@@ -11,7 +11,7 @@ public class ProductValidator {
     public void validInputPrice(PriceDTO priceSearchParams) throws InvalidDatesException, NullValueException {
 
         if ((priceSearchParams.getStartDate() != null && priceSearchParams.getEndDate() != null) && priceSearchParams.getStartDate().after(priceSearchParams.getEndDate())) {
-            throw new InvalidDatesException("start date must be greater than end date");
+            throw new InvalidDatesException("start date must be before than end date");
         }
     }
 }
